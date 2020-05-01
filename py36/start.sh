63cdfb25d42a96f74483c04d64c169c174538fd6
fazilhero <fazil.topal@deliveryhero.com>
2020-05-01 16:04:47 +0200

    [ADD] First docker image drafts

---
+++ py36/start.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+set -e
+
+echo "conda version: $(conda --version)"
+
+python -c 'import sys; print("python version: {}.{}".format(sys.version_info.major, sys.version_info.minor))'
+
---
+++ py37/start.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env sh
+set -e
+
+echo "conda version: $(conda --version)"
+
+python -c 'import sys; print("python version: {}.{}".format(sys.version_info.major, sys.version_info.minor))'
